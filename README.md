3b8d9192dcc1bcf37cbac087ef3e8c6cb24da415
Azharsayyed5 <55562412+Azharsayyed5@users.noreply.github.com>
2019-10-28 15:34:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DJANGO-REST-FRAMEWORK-CRUDE-APPLICATION
+ Simple crude application developed using Django & REST framework
---
+++ todos/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+#userbname : aarizs pw: 9004861590
+# Register your models here.
+from .models import Todo
+
+admin.site.register(Todo)
